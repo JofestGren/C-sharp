694c0a49dd98350714344c310260a85426cf2166
Jofest Gren <jofest.g.70@gmail.com>
2017-11-01 18:56:30 +0200

    first practical v1.0.2

---
+++ HappyTicket/HappyTicket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HappyTicket
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Random rand = new Random ();
+			int n = 6;
+			string s = "";
+
+			for (int i = 0; i < n; i++) 
+			{
+				s += Convert.ToString (rand.Next (9) + " ");
+			}
+
+			Console.WriteLine ("{0} ", s);
+			int left = StrToInt(s.Remove (6));
+			int right = StrToInt(s.Remove (0, 6));
+
+			if (left== right)
+				Console.WriteLine ("Happy ticket!");
+			else
+				Console.WriteLine ("Regular ticket!");
+			Console.Write ("Left: {0}, Right: {1} ", left, right);
+		}
+
+		static int StrToInt (string str)
+		{
+			str = str.Trim ().Replace (" ", string.Empty);
+			int num = Convert.ToInt32 (str, 10);
+			return num;
+		}
+	}
+}
---
+++ MultiTable/MultiTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MultiTable
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write ("y/x |  ");
+			for (int k = 1; k <= 10; k++)
+				Console.Write ("{0,4} ", k);
+			Console.Write ("\n------------------------------------------------------------");
+			Console.WriteLine ();
+			for (int i = 1; i <= 10; i++) 
+			{
+				Console.Write ("{0,3} |  ", i);
+				for (int j = 1; j <= 10; j++)
+					Console.Write ("{0,4} ", i * j);
+				Console.WriteLine ("");
+				Console.WriteLine ("");
+			}
+			
+		}
+	}
+}
